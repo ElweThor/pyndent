ae23411fcc5d1967d6baa9676201b67986c28f96
ElweThor <elwe.thor@gmail.com>
2025-10-04 20:25:38 +0200

    pyndent.bat bugfix, to be able to launch it from everywhere in the system (Win/DOS need only)

--- src/pyndent.bat
+++ src/pyndent.bat
@@ -3,5 +3,5 @@ REM
 REM PYndent last stable version - The Python re-Indent Pre-processor
 REM (C)2025 by Elwe Thor - Aria@DeepSeek - CC-BY-NC-SA 4.0
 REM
-py pyndent.py %*
+py "%~dp0\pyndent.py" %*
 if errorlevel 1 echo Pyndent completed with errors - RC: %errorlevel% & exit /b %errorlevel%
